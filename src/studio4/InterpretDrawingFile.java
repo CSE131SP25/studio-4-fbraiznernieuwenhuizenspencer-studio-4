fd52bbf6d9455226fe7304d1e17655d5377a0f38
ameliaspencer <ameliaspencer@localhost>
2025-02-19 14:09:17 -0600

    draw

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -19,6 +19,47 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		String Shape = in.next();
+		int one = in.nextInt();
+		int two = in.nextInt();
+		int three = in.nextInt();
+		boolean filled = in.nextBoolean();
 		
+		if (Shape.equals("rectangle") == true) {
+			if (filled == true) {
+				StdDraw.setPenColor(255, 109, 182);
+				StdDraw.filledRectangle(.5, 0.5, 0.2, 0.2);
+			}
+			else {
+				StdDraw.setPenColor(0,0,0);
+				StdDraw.rectangle(0.5,0.5,0.2,0.2);
+			}
+		}
+		else if (Shape.equals("ellipse") == true) {
+			if (filled == true) {
+				StdDraw.setPenColor(146, 0, 0);
+				StdDraw.filledEllipse(0.5, 0.5, 0.2, 0.2);
+			}
+			else {
+				StdDraw.setPenColor(73, 0, 146);
+				StdDraw.ellipse(0.5, 0.5, 0.2, 0.2);
+			}
+		}
+		else {
+			if (filled == true) {
+				double[] x = {0.2, 0.8, 0.5};
+				double[] y = {0.2, 0.2, 0.5};
+				
+				StdDraw.setPenColor(36, 255, 36);
+				StdDraw.filledPolygon(x, y);
+			}
+			else {
+				double[] x = {0.2, 0.8, 0.5};
+				double[] y = {0.2, 0.2, 0.5};
+				
+				StdDraw.setPenColor(109, 182, 255);
+				StdDraw.polygon(x, y);
+			}
+		}
 	}
 }
